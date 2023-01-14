5773799af2b91c798c6bacc54e78f51f5901b115
Alexandra Demchenko <demchenko345@gmail.com>
2023-01-14 21:28:24 +0300

    Version 1.2

--- DMSO.py
+++ DMSO.py
@@ -4,11 +4,49 @@ import matplotlib.pyplot as plt
 
 Tc = 706.9
 Pc = 58.5 / 10  # bar to MPa
-W = 0.45
+W = 0.35
 M = 78.13
 
 fluid = PengRobinson(Tc, Pc, W)
 
+# density at p = 0.1Mpa
+P = np.array([0.1])
+density = []
+T = list(np.arange(293, 364))
+for t in T:
+    density.append(max(list(fluid.density_calc(P, t))))
+print(density)
+data_agieienko = np.array(
+    # T,k ρ, kg/m^3
+    [[308.15, 1085.22],
+     [313.15, 1080.2],
+     [318.15, 1075.18],
+     [323.15, 1070.16],
+     [328.15, 1065.14],
+     [333.15, 1060.12],
+     [338.15, 1055.1],
+     [343.15, 1050.07],
+     [348.15, 1045.04],
+     [353.15, 1040],
+     [358.15, 1034.95],
+     [363.15, 1029.9]])
+data_yue = np.array([[293.15, 1.1004],
+                     [298.15, 1.0956],
+                     [303.15, 1.0905],
+                     [308.15, 1.0854],
+                     [313.15, 1.0804],
+                     [318.15, 1.0759]])
+data_agieienko[:, 1] = data_agieienko[:, 1] / M
+data_yue[:, 1] = data_yue[:, 1] * 1000 / M
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.plot(data_agieienko[:, 1], data_agieienko[:, 0], '*', label='agienko-2020')
+ax.plot(data_yue[:, 1], data_yue[:, 0], 'o', label='yue-2018')
+ax.plot(density, T, color='k', label='comp')
+
+ax.set(ylabel='T, K', xlabel='ρ, mol\l')
+ax.legend()
+plt.legend(frameon=True)
 # pressure
 n = 100
 # 1st part
@@ -26,8 +64,18 @@ T_0_3 = np.linspace(406, 356, num=n)
 P_0_3 = np.linspace(0.018, 0.002, num=n)
 root3 = fluid.pressure_calc_roots(P_0_3, T_0_3)
 
-T_sat = np.concatenate((T_0_1, T_0_2, T_0_3))
-p_sat = np.concatenate((root1, root2, root3))
+# 4th part
+T_0_4 = np.linspace(356, 330, num=n)
+P_0_4 = np.linspace(0.002, 0.00067, num=n)
+root4 = fluid.pressure_calc_roots(P_0_4, T_0_4)
+
+# 5th part
+T_0_5 = np.linspace(330, 300, num=n)
+P_0_5 = np.linspace(0.00067, 0.00008, num=n)
+root5 = fluid.pressure_calc_roots(P_0_5, T_0_5)
+
+T_sat = np.concatenate((T_0_1, T_0_2, T_0_3, T_0_4, T_0_5))
+p_sat = np.concatenate((root1, root2, root3, root4, root5))
 
 data_tochigi = np.array([
     # T,K   p,kPa
@@ -52,10 +100,41 @@ data_tochigi = np.array([
     [363.15, 3.28],
     [356.23, 2.44]])
 data_tochigi[:, 1] = data_tochigi[:, 1] / 1000
-
+data_nishimura = np.array(
+    # T,C p,mmHg
+    [[27.1, 0.67],
+     [31.8, 0.97],
+     [38.8, 1.5],
+     [46.9, 2.78],
+     [47.7, 2.85],
+     [50, 3.21],
+     [54, 4.14],
+     [55.6, 4.39],
+     [56.9, 4.94],
+     [58, 5.35],
+     [61.1, 6.12],
+     [70.9, 10.19],
+     [77, 13.74],
+     [86.1, 21.32],
+     [93.9, 30.08],
+     [101.9, 42.19],
+     [107.7, 53.41],
+     [113.4, 66.71],
+     [117.5, 77.89],
+     [121.8, 91.16],
+     [125, 102.7],
+     [130, 122.4],
+     [134.8, 146.3],
+     [140, 174.2],
+     [144.8, 204.7],
+     [150.2, 239.5],
+     ])
+data_nishimura[:, 0] = data_nishimura[:, 0] + 273.15
+data_nishimura[:, 1] = data_nishimura[:, 1] * 0.000133322
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(data_tochigi[:, 0], data_tochigi[:, 1], 'o', label='tochigi-1999')
+ax.plot(data_nishimura[:, 0], data_nishimura[:, 1], '*', label='nishimura-1972')
 ax.plot(T_sat, p_sat, color='k', label='comp')
 ax.set(xlabel='T, K', ylabel='p, MPa')
 ax.legend()
